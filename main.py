a0a00d662bedc728f869775d5652234a3178954c
Daniella-13-gif <ngozihomestore@gmail.com>
2025-03-16 13:00:55 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+def intro(name):
+
+    print("Hello,good day. I am", name)
+    print("I am here to help you with your queries",name)
+
+    user_name = input("Enter your name")
+    intro(user_name)
